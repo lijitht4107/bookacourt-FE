1cf5341160761b6d75c6a5c7eae103382f9f5e30
lijitht4107 <lijiththazhathethil@gmail.com>
2024-07-21 00:48:46 +0530

    env modified

--- src/Components/SighnupBox.jsx
+++ src/Components/SighnupBox.jsx
@@ -1,87 +1,85 @@
 import {MDBInput,MDBIcon}from 'mdb-react-ui-kit';
 import React, { useState } from 'react'
-import axios from 'axios';
-import { BASE_URL } from '../Constants/Const';
-
+import AxiosInstance from '../Config/AxiosInstants';
 
 
 function SighnupBox({ setBoxName }) {
+  const handleLogin = () => {
+    setBoxName("login");
+  };
 
-    const handleLogin = () => {
-     
-      setBoxName("login");
-    };
-  
-    const [signupData, setSignupData] = useState({
-      fName: "",
-      lName: "",
-      email: "",
-      password: "",
-      conPass: "",
-    });
-  
-    const handleRegister = (e) => {
-      try {
-        e.preventDefault();
-        axios.post(`${BASE_URL}/auth/signup`, signupData).then((res) => {
-          if (res.data.message === "signup data successfull") {
-            setBoxName("login");
-          }
-  
-          if (res.data.message === "email already exist") {
-            alert("email already exist");
-           }
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    };
+  const [signupData, setSignupData] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    password: "",
+    conPass: "",
+  });
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    AxiosInstance.post('/auth/signup', signupData)
+      .then((res) => {
+        if (res.data.message === "signup data successfull") {
+          setBoxName("login");
+        } else if (res.data.message === "email already exists") {
+          alert("Email already exists");
+        } else {
+          alert("Unexpected response: " + res.data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error with the signup request:", error.response || error.message || error);
+        alert("There was an error with the signup request: " + (error.response ? error.response.data.message : error.message));
+      });
+  };
+
   return (
     <>
-    <div className="d-flex flex-row align-items-center mb-4 ">
-          <MDBIcon fas icon="user me-3" size='lg'/>
-          <MDBInput label='first Name'  type='text' className='w-100' required  value={signupData.fName}
-                      onChange={(e) => {
-                        setSignupData({ ...signupData, fName: e.target.value });
-                      }}/>
-        </div>
+      <div className="d-flex flex-row align-items-center mb-4">
+        <MDBIcon fas icon="user me-3" size='lg' />
+        <MDBInput label='First Name' type='text' className='w-100' required value={signupData.fName}
+          onChange={(e) => {
+            setSignupData({ ...signupData, fName: e.target.value });
+          }} />
+      </div>
 
-        <div className="d-flex flex-row align-items-center mb-4 ">
-          <MDBIcon fas icon="user me-3" size='lg'/>
-          <MDBInput label='Last Name'  type='text' className='w-100' required  onChange={(e) => {
-                        setSignupData({ ...signupData, lName: e.target.value });
-                      }}/>
-        </div>
+      <div className="d-flex flex-row align-items-center mb-4">
+        <MDBIcon fas icon="user me-3" size='lg' />
+        <MDBInput label='Last Name' type='text' className='w-100' required value={signupData.lName}
+          onChange={(e) => {
+            setSignupData({ ...signupData, lName: e.target.value });
+          }} />
+      </div>
 
-        <div className="d-flex flex-row align-items-center mb-4">
-          <MDBIcon fas icon="envelope me-3" size='lg'/>
-          <MDBInput label='Your Email'  type='email' required  onChange={(e) => {
-                    setSignupData({ ...signupData, email: e.target.value });
-                  }}/>
-        </div>
+      <div className="d-flex flex-row align-items-center mb-4">
+        <MDBIcon fas icon="envelope me-3" size='lg' />
+        <MDBInput label='Your Email' type='email' required value={signupData.email}
+          onChange={(e) => {
+            setSignupData({ ...signupData, email: e.target.value });
+          }} />
+      </div>
 
-        <div className="d-flex flex-row align-items-center mb-4">
-          <MDBIcon/>
-          <MDBInput label='Password' id='form3' type='password' required onChange={(e) => {
-                    setSignupData({ ...signupData, password: e.target.value });
-                  }}/>
-        </div>
+      <div className="d-flex flex-row align-items-center mb-4">
+        <MDBIcon />
+        <MDBInput label='Password' type='password' required value={signupData.password}
+          onChange={(e) => {
+            setSignupData({ ...signupData, password: e.target.value });
+          }} />
+      </div>
 
-        <div className="d-flex flex-row align-items-center mb-4">
-          <MDBIcon />
-          <MDBInput label='Repeat your password' id='form4' type='password' required onChange={(e) => {
-                    setSignupData({ ...signupData, conPass: e.target.value });
-                  }}/>
-        </div>
+      <div className="d-flex flex-row align-items-center mb-4">
+        <MDBIcon />
+        <MDBInput label='Repeat your password' type='password' required value={signupData.conPass}
+          onChange={(e) => {
+            setSignupData({ ...signupData, conPass: e.target.value });
+          }} />
+      </div>
 
-      
-        <button className="mb-4 btn btn-primary" size="lg" onClick={handleRegister}>Signup</button>
-        <p className="text-center mb-5 mx-1 mx-md-4 mt-4" onClick={handleLogin}>Login</p>
-
-
-
+      <button className="mb-4 btn btn-primary" size="lg" onClick={handleRegister}>Signup</button>
+      <p className="text-center mb-5 mx-1 mx-md-4 mt-4" onClick={handleLogin}>Login</p>
     </>
-  )
+  );
 }
 
-export default SighnupBox
+export default SighnupBox;
--- src/Constants/Const.js
+++ src/Constants/Const.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_URL;
+export const BASE_URL = 'http://localhost:4000';
 
 
 export const TIMINGS = [{
